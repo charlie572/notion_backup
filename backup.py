fce0dc3c90838b5aa33c98e56454e435a555df42
Charlie Bareham <barehamcharlie@gmail.com>
2023-12-27 17:41:59 +0000

    Download export

--- backup.py
+++ backup.py
@@ -1,3 +1,6 @@
+from configparser import ConfigParser
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver import FirefoxProfile
 from selenium.webdriver.common.by import By
@@ -5,13 +8,7 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.options import Options
 
 
-def main():
-    # instantiate driver
-    options = Options()
-    options.binary = FirefoxBinary("/snap/firefox/current/usr/lib/firefox/firefox")
-    options.profile = FirefoxProfile("/home/charlie/snap/firefox/common/.mozilla/firefox/zcff3mio.Bot")
-    driver = webdriver.Firefox(options=options)
-
+def start_notion_export(driver):
     # open notion
     driver.get("https://www.notion.so/login")
 
@@ -29,7 +26,40 @@ def main():
     export_button_2 = driver.find_element(by=By.XPATH, value="//*[text()='Export']")
     export_button_2.click()
 
+
+def download_notion_export(driver):
+    # open notion
+    driver.get("https://www.notion.so/login")
+
+    driver.implicitly_wait(10.0)
+
+    # open updates sidebar
+    updates_icon = driver.find_element(by=By.CLASS_NAME, value="sidebarUpdates")
+    updates_icon.click()
+
+    # click download button
+    download_button = driver.find_element(by=By.XPATH, value="//*[text()='Download']")
+    download_button.click()
+
+
+def main():
+    parser = ConfigParser()
+    parser.read("config.ini")
+
+    # instantiate driver
+    options = Options()
+    options.binary = FirefoxBinary(parser.get("firefox", "executable"))
+    options.profile = FirefoxProfile(parser.get("firefox", "profile"))
+    driver = webdriver.Firefox(options=options)
+
+    start_notion_export(driver)
     driver.close()
+    sleep(60 * 20)
+
+    driver = webdriver.Firefox(options=options)
+    download_notion_export(driver)
+    driver.close()
+    sleep(60 * 5)
 
 
 if __name__ == "__main__":
